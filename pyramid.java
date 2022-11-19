cf721c33d4c550413cfdc2cf988d62981919a6d9
ms-joker <90680241+ms-joker@users.noreply.github.com>
2022-11-19 23:24:14 +0300

    Update pyramid.java
    I forgot there is a repeat method, so I rewrote printing space.

--- pyramid.java
+++ pyramid.java
@@ -8,10 +8,7 @@ class Main {
     int n = input.nextInt();
 
     for (int i=1; i<=n; i ++){ 
-      int a = 2*i-1;
-      for (int j=2*(n-i); j>0; j--){
-        System.out.print(" ");
-      }      
+      System.out.print(" ".repeat(2*(n-i)));      
       for (int j=1; j<=i; j++){
         System.out.print((i-j+1) + " ");
       }

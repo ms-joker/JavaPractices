c526c34f87a8755c9e6073e49f042cb6284d7ed2
ms-joker <90680241+ms-joker@users.noreply.github.com>
2022-12-05 23:08:28 +0300

    Update pyramid.java
    Simplified a little bit. 💯

--- pyramid.java
+++ pyramid.java
@@ -9,8 +9,8 @@ class Main {
 
     for (int i=1; i<=n; i ++){ 
       System.out.print(" ".repeat(2*(n-i)));      
-      for (int j=1; j<=i; j++){
-        System.out.print((i-j+1) + " ");
+      for (int j=0; j<i; j++){
+        System.out.print((i-j) + " ");
       }
       for (int k=2; k<i+1; k++){
         System.out.print(k + " ");
